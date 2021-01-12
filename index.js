cc0396cbc4748fb9649d773daa14edcd6cdce51a
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-12 12:14:19 -0800

    passing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let notes = []
+  for (let i = 0; i < names.length; i++) {
+    notes.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return notes
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--)
+  }
+}
